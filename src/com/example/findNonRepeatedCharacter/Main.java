4362b50c650fcc968f651433f8edfb60aa9b0c31
akash1521439 <mr.akash016@gmail.com>
2024-02-19 14:21:11 -0600

    Updates. No repeated characters easy and short way.

--- src/com/example/findNonRepeatedCharacter/Main.java
+++ src/com/example/findNonRepeatedCharacter/Main.java
@@ -8,19 +8,23 @@ import java.util.Map;
 public class Main {
     public static char findNonRepeatedCharacter(String str) {
 
-        Map<Character,Long> charCount = str.chars()
-                .mapToObj(c -> (char) c)
-                .collect(LinkedHashMap::new,
-                        (mymap,chr) -> mymap.put(chr, mymap.containsKey(chr) ? mymap.get(chr) + 1 : 1),LinkedHashMap::putAll);
+//        Map<Character,Long> charCount = str.chars()
+//                .mapToObj(c -> (char) c)
+//                .collect(LinkedHashMap::new,
+//                        (mymap,chr) -> mymap.put(chr, mymap.containsKey(chr) ? mymap.get(chr) + 1 : 1),LinkedHashMap::putAll);
+//
+//        return charCount.entrySet().stream().filter(entry -> entry.getValue() == 1)
+//                .map(Map.Entry::getKey)
+//                .findAny()
+//                .orElseThrow(()-> new RuntimeException("No non-repeating character found!"));
 
-        return charCount.entrySet().stream().filter(entry -> entry.getValue() == 1)
-                .map(Map.Entry::getKey)
-                .findAny()
-                .orElseThrow(()-> new RuntimeException("No non-repeating character found!"));
+       Character ccc = str.chars().mapToObj(c -> (char) c).filter(c -> str.toLowerCase().chars()
+                .filter(ca -> ca == c).count() == 1).findFirst().orElse(null);
+       return ccc;
     }
 
     public static void main(String[] args){
-        String string = "Java articles are Awesome";
+        String string = "Jjava articles are Awesome";
         System.out.println("First non-repeating character is: "+findNonRepeatedCharacter(string));
     }
 }
